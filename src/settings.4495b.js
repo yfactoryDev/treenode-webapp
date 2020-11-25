1e8401f1af70f63bdf590f9d41d0d82599b758b4
tiger <wangzhen00@gmail.com>
2020-11-25 15:09:03 +0900

    - added movie for MatchEffectSetting - fixed a fews bug

---
+++ src/settings.4495b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Loading/LoadingScene.fire",orientation:"portrait",jsList:["assets/libs/pickerView.style.min.c0763.js"],bundleVers:{internal:"45e63",resources:"eb279",main:"527b7"}};
