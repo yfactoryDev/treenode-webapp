566723c9cfdf1850f8addde1fdb92e7051f8a83f
tiger <wangzhen00@gmail.com>
2020-12-11 12:16:22 +0900

    - fixed feedback #20201201

---
+++ src/settings.64d0b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Loading/LoadingScene.fire",orientation:"portrait",jsList:["assets/libs/pickerView.style.min.c0763.js"],bundleVers:{internal:"45e63",resources:"e0ff0",main:"5d9d6"}};
