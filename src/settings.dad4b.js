c1eb67444a90ddec6d0b74d561bdb45c3b3c72cd
tiger <wangzhen00@gmail.com>
2021-01-08 19:00:26 +0900

    no message

--- src/settings.dad4b.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Loading/LoadingScene.fire",orientation:"portrait",jsList:["assets/libs/pickerView.style.min.c0763.js"],bundleVers:{internal:"45e63",resources:"9fc86",main:"b0a90"}};
